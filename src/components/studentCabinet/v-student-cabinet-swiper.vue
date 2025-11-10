01f068fbbde3f4cc035af76fd7cda201113edb2f
Martin <ppirks@gmail.com>
2025-11-10 13:12:07 +0300

    Fix: url answer in student cabinet

--- src/components/studentCabinet/v-student-cabinet-swiper.vue
+++ src/components/studentCabinet/v-student-cabinet-swiper.vue
@@ -2,69 +2,19 @@
   <div class="v-student-cabinet-swiper">
     <div class="flex gap-3 items-center v-student-cabinet-swiper__buttons">
       <template v-for="type in ['rotate-180', '']" :key="type">
-        <div
-          :class="`${type} custom-swiper-button day-el student-swiper-button-${type.length ? 'prev' : 'next'}`"
-        >
-          <svg
-            width="40"
-            height="40"
-            viewBox="0 0 40 40"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="39.5"
-              y="0.5"
-              width="39"
-              height="39"
-              rx="19.5"
-              transform="rotate(90 39.5 0.5)"
-              fill="#F3F4F6"
-            />
-            <rect
-              x="39.5"
-              y="0.5"
-              width="39"
-              height="39"
-              rx="19.5"
-              transform="rotate(90 39.5 0.5)"
-              stroke="#F3F4F6"
-            />
-            <path
-              d="M15.7578 20.0001H24.2427M24.2427 20.0001L20.0003 15.7577M24.2427 20.0001L20.0003 24.2425"
-              stroke="#717680"
-              stroke-width="1.85556"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
+        <div :class="`${type} custom-swiper-button day-el student-swiper-button-${type.length ? 'prev' : 'next'}`">
+          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="39.5" y="0.5" width="39" height="39" rx="19.5" transform="rotate(90 39.5 0.5)" fill="#F3F4F6" />
+            <rect x="39.5" y="0.5" width="39" height="39" rx="19.5" transform="rotate(90 39.5 0.5)" stroke="#F3F4F6" />
+            <path d="M15.7578 20.0001H24.2427M24.2427 20.0001L20.0003 15.7577M24.2427 20.0001L20.0003 24.2425"
+              stroke="#717680" stroke-width="1.85556" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         </div>
-        <div
-          :class="`${type} custom-swiper-button night-el student-swiper-button-${type.length ? 'prev' : 'next'}`"
-        >
-          <svg
-            width="40"
-            height="40"
-            viewBox="0 0 40 40"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="40"
-              width="40"
-              height="40"
-              rx="20"
-              transform="rotate(90 40 0)"
-              fill="white"
-              fill-opacity="0.08"
-            />
-            <path
-              d="M15.7573 20.0001H24.2422M24.2422 20.0001L19.9998 15.7577M24.2422 20.0001L19.9998 24.2425"
-              stroke="#E9EAEB"
-              stroke-width="1.85556"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
+        <div :class="`${type} custom-swiper-button night-el student-swiper-button-${type.length ? 'prev' : 'next'}`">
+          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="40" width="40" height="40" rx="20" transform="rotate(90 40 0)" fill="white" fill-opacity="0.08" />
+            <path d="M15.7573 20.0001H24.2422M24.2422 20.0001L19.9998 15.7577M24.2422 20.0001L19.9998 24.2425"
+              stroke="#E9EAEB" stroke-width="1.85556" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         </div>
       </template>
@@ -79,16 +29,10 @@
       <p>Current homework ID: {{ currentHomework?.id || 'None' }}</p>
       <p>Answer files count: {{ answer.length }}</p>
       <p>Cache status: {{ cacheStatus }}</p>
-      <button
-        @click="() => clearCache(props.profile?.id)"
-        class="mt-2 px-2 py-1 bg-red-500 text-white text-xs rounded"
-      >
+      <button @click="() => clearCache(props.profile?.id)" class="mt-2 px-2 py-1 bg-red-500 text-white text-xs rounded">
         Clear Cache
       </button>
-      <button
-        @click="refreshData"
-        class="mt-2 ml-2 px-2 py-1 bg-blue-500 text-white text-xs rounded"
-      >
+      <button @click="refreshData" class="mt-2 ml-2 px-2 py-1 bg-blue-500 text-white text-xs rounded">
         Refresh
       </button>
     </div>
@@ -102,19 +46,10 @@
     </div>
     <!-- Swiper только если есть уроки -->
     <template v-else>
-      <swiper
-        class="v-student-cabinet-swiper__prev-row"
-        ref="swiperRef"
-        @swiper="onSwiper"
-        @slide-change="onSlideChange"
-        v-bind="swiperOptions"
-        :modules="modules"
-      >
-        <swiper-slide
-          class="v-student-cabinet-swiper__prev-item lesson-swiper"
-          v-for="lesson in lessons"
-          :key="lesson.id"
-        >
+      <swiper class="v-student-cabinet-swiper__prev-row" ref="swiperRef" @swiper="onSwiper"
+        @slide-change="onSlideChange" v-bind="swiperOptions" :modules="modules">
+        <swiper-slide class="v-student-cabinet-swiper__prev-item lesson-swiper" v-for="lesson in lessons"
+          :key="lesson.id">
           <div class="lesson-swiper__content">
             <h3 class="lesson-swiper__date">Занятие от {{ formatDate(lesson.date) }}</h3>
             <div class="lesson-swiper__info">
@@ -122,24 +57,14 @@
                 <h4 class="lesson-swiper__title">Темы</h4>
                 <div class="lesson-swiper__block">
                   <template v-if="lesson.topics && lesson.topics.length">
-                    <div
-                      class="lesson-swiper__theme theme"
-                      @click="() => openModal('themes', lesson.id)"
-                      v-for="theme in lesson.topics"
-                      :key="theme.id"
-                    >
+                    <div class="lesson-swiper__theme theme" @click="() => openModal('themes', lesson.id)"
+                      v-for="theme in lesson.topics" :key="theme.id">
                       <span class="theme__title">{{ theme.name }}</span>
                     </div>
                   </template>
                   <template v-else>
                     <div class="theme" @click="openModal('themes', lesson.id)">
-                      <svg
-                        width="12"
-                        height="12"
-                        viewBox="0 0 12 12"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
+                      <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0 5H5V0H7V5H12V7H7V12H5V7H0V5Z" fill="#1d4ecc" />
                       </svg>
                       <span class="theme__title"> Добавить тему</span>
@@ -151,11 +76,7 @@
                 <h4 class="lesson-swiper__title">Заметки преподавателя</h4>
                 <div class="lesson-swiper__block lesson-swiper__col start">
                   <template v-if="lesson.problems && lesson.problems.length">
-                    <p
-                      v-for="problem in lesson.problems"
-                      :key="problem.id || problem"
-                      class="lesson-swiper__subtitle"
-                    >
+                    <p v-for="problem in lesson.problems" :key="problem.id || problem" class="lesson-swiper__subtitle">
                       {{ problem.problem_text }}.
                     </p>
                   </template>
@@ -166,42 +87,19 @@
               </div>
               <div class="lesson-swiper__info-row">
                 <h4 class="lesson-swiper__title">Прикреплённое ДЗ</h4>
-                <div
-                  class="lesson-swiper__block col"
-                  v-if="lesson.homeworks && lesson.homeworks.length"
-                >
+                <div class="lesson-swiper__block col" v-if="lesson.homeworks && lesson.homeworks.length">
                   <p class="lesson-swiper__subtitle">{{ lesson.homeworks[0].title }}</p>
-                  <p
-                    class="lesson-swiper__subtitle"
-                    :class="getDeadlineClass(lesson.homeworks[0].due_date)"
-                  >
+                  <p class="lesson-swiper__subtitle" :class="getDeadlineClass(lesson.homeworks[0].due_date)">
                     Дедлайн {{ formatDate(lesson.homeworks[0].due_date) }}
                   </p>
                   <div class="lesson-swiper__homework">
-                    <v-files-handler
-                      :model-value="transformedFiles(lesson.homeworks[0].files)"
-                      :deletable="false"
-                    />
-                    <button
-                      class="lesson-swiper__button custom-btn blue"
-                      @click="loadAnswer(lesson.homeworks[0].id)"
-                      v-if="!getStudentFilesAnswer(lesson).length"
-                      й
-                    >
-                      <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
+                    <v-files-handler :model-value="transformedFiles(lesson.homeworks[0].files)" :deletable="false" />
+                    <button class="lesson-swiper__button custom-btn blue" @click="loadAnswer(lesson.homeworks[0].id)"
+                      v-if="!getStudentFilesAnswer(lesson).length" й>
+                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                           d="M11.6667 1.66669H5.00004C4.55801 1.66669 4.13409 1.84228 3.82153 2.15484C3.50897 2.4674 3.33337 2.89133 3.33337 3.33335V16.6667C3.33337 17.1087 3.50897 17.5326 3.82153 17.8452C4.13409 18.1578 4.55801 18.3334 5.00004 18.3334H15C15.4421 18.3334 15.866 18.1578 16.1786 17.8452C16.4911 17.5326 16.6667 17.1087 16.6667 16.6667V6.66669M11.6667 1.66669L16.6667 6.66669M11.6667 1.66669V6.66669H16.6667M10 15V10M7.50004 12.5H12.5"
-                          stroke="white"
-                          stroke-width="1.67"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
+                          stroke="white" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
                       </svg>
                       Загрузить ответ
                     </button>
@@ -211,27 +109,17 @@
               <div class="lesson-swiper__info-row" v-if="getStudentFilesAnswer(lesson).length">
                 <h4 class="lesson-swiper__title">Ваш ответ</h4>
                 <div class="lesson-swiper__block lesson-swiper__answer nowrap">
-                  <v-files-handler
-                    :model-value="getStudentFilesAnswer(lesson)"
-                    :deletable="false"
-                  />
-                  <img
-                    src="/src/assets/images/pen.svg"
-                    alt=""
-                    @click="editHomework(lesson.homeworks[0].id)"
-                  />
+                  <v-files-handler :model-value="getStudentFilesAnswer(lesson)" :deletable="false" />
+                  <img src="/src/assets/images/pen.svg" alt="" @click="editHomework(lesson.homeworks[0].id)" />
                 </div>
               </div>
               <div class="lesson-swiper__info-row" v-if="getStudentSubmissions(lesson)">
                 <h4 class="lesson-swiper__title">Оценка</h4>
                 <div class="lesson-swiper__block lesson-swiper__answer">
-                  <div
-                    class="status"
-                    :class="{
-                      green: getStudentSubmissions(lesson).grade >= 4,
-                      red: getStudentSubmissions(lesson).grade < 4,
-                    }"
-                  >
+                  <div class="status" :class="{
+                    green: getStudentSubmissions(lesson).grade >= 4,
+                    red: getStudentSubmissions(lesson).grade < 4,
+                  }">
                     {{ getStudentSubmissions(lesson).grade || 'Без оценки' }}
                   </div>
                   {{ getStudentSubmissions(lesson).feedback }}
@@ -464,13 +352,17 @@ function getStudentFilesAnswer(lesson) {
       props.currentHomeworkId &&
       homework.id == props.currentHomeworkId
     ) {
-      log('Returning props.answer for homework:', homework.id, props.answer)
-      return props.answer.map((item) => ({
-        id: item.id,
-        name: item.name,
-        size: item.size,
-        file: `${domain}${item.file}`,
-      }))
+
+      const result = homework.student_answers.map((item) => {
+        return {
+          id: item.id,
+          name: item.file_name,
+          size: item.file_size,
+          file: `${domain}${item.token_file_url}`,
+        }
+      })
+
+      return result;
     }
     if (homework.student_answers && homework.student_answers.length > 0) {
       log('Returning saved answers for homework:', homework.id, homework.student_answers)
