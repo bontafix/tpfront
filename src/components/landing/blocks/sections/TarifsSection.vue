1c3848a4b8d2f02990a6cdc9da7c92ab2d4062b7
Daniil Pavlov <foxy09876@yandex.ru>
2025-11-19 19:00:35 +0000

    SEO изменения #1 *Начало переработки страниц для SEO, изменение элементов и циклов, чтобы добавить SEO элементы (role, подсказки и др.)

--- src/components/landing/blocks/sections/TarifsSection.vue
+++ src/components/landing/blocks/sections/TarifsSection.vue
@@ -19,7 +19,7 @@ function openFreeServiceOverlay() {
 }
 
 function closeFreeServiceOverlay() {
-  showFreeServiceOverlay.value = true
+  showFreeServiceOverlay.value = false
 }
 
 onMounted(() => {
@@ -28,20 +28,21 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="tarifs-section" id="tarifs">
-    <div class="tarifs-section__badge">Тарифы</div>
-    <h2 class="section-title inter-500 tarifs-section__title">
+  <section class="tarifs-section" id="tarifs" itemscope itemtype="https://schema.org/Product">
+    <div class="tarifs-section__badge" itemprop="category">Тарифы</div>
+    <h2 class="section-title inter-500 tarifs-section__title" itemprop="name">
       Подберите тариф по количеству учеников и увеличивайте их число вместе с ТР
     </h2>
 
     <div class="tarifs-section__container">
       <div class="tarifs-section__tarifs" :class="{ 'tarifs-section__tarifs--blurred': showFreeServiceOverlay }">
-        <div class="tarifs-section__tarif">
+
+        <div class="tarifs-section__tarif" itemprop="offers" itemscope itemtype="https://schema.org/Offer">
           <div class="rounded-as-icon">
-            <img src="@/assets/icons/checkbox2.svg" alt="Free" />
+            <img src="@/assets/icons/checkbox2.svg" alt="Базовый тариф" itemprop="image"/>
           </div>
-          <p class="tarifs-section__tariftitle">Базовый</p>
-          <h3 class="tarifs-section__func-title">Бесплатно</h3>
+          <p class="tarifs-section__tariftitle" itemprop="name">Базовый</p>
+          <h3 class="tarifs-section__func-title" itemprop="price">Бесплатно</h3>
 
           <div class="tarifs-section__tarif-features">
             <CheckboxItem description="До 5 учеников" />
@@ -53,12 +54,12 @@ onMounted(() => {
           <button class="primary-button" @click="pushToRegister">Начать работу</button>
         </div>
 
-        <div class="tarifs-section__tarif tarifs-section__tarif--featured">
+        <div class="tarifs-section__tarif tarifs-section__tarif--featured" itemprop="offers" itemscope itemtype="https://schema.org/Offer">
           <div class="rounded-as-icon">
-            <img src="@/assets/icons/checkbox2.svg" alt="Free" />
+            <img src="@/assets/icons/checkbox2.svg" alt="Продвинутый тариф" itemprop="image"/>
           </div>
-          <p class="tarifs-section__tariftitle">Продвинутый</p>
-          <h3 class="tarifs-section__func-title">399 ₽ / месяц</h3>
+          <p class="tarifs-section__tariftitle" itemprop="name">Продвинутый</p>
+          <h3 class="tarifs-section__func-title" itemprop="price">399 ₽ / месяц</h3>
 
           <div class="tarifs-section__tarif-features">
             <CheckboxItem description="До 12 учеников" />
@@ -71,12 +72,12 @@ onMounted(() => {
           <button class="secondary-button" @click="openFreeServiceOverlay">Выбрать</button>
         </div>
 
-        <div class="tarifs-section__tarif">
+        <div class="tarifs-section__tarif" itemprop="offers" itemscope itemtype="https://schema.org/Offer">
           <div class="rounded-as-icon">
-            <img src="@/assets/icons/checkbox2.svg" alt="Free" />
+            <img src="@/assets/icons/checkbox2.svg" alt="Эксперт тариф" itemprop="image"/>
           </div>
-          <p class="tarifs-section__tariftitle">Эксперт</p>
-          <h3 class="tarifs-section__func-title">599 ₽ / месяц</h3>
+          <p class="tarifs-section__tariftitle" itemprop="name">Эксперт</p>
+          <h3 class="tarifs-section__func-title" itemprop="price">599 ₽ / месяц</h3>
 
           <div class="tarifs-section__tarif-features">
             <CheckboxItem description="Количество учеников неограничено" />
@@ -90,12 +91,12 @@ onMounted(() => {
           <button class="primary-button" @click="openFreeServiceOverlay">Выбрать</button>
         </div>
 
-        <div class="tarifs-section__tarif">
+        <div class="tarifs-section__tarif" itemprop="offers" itemscope itemtype="https://schema.org/Offer">
           <div class="rounded-as-icon">
-            <img src="@/assets/icons/checkbox2.svg" alt="Free" />
+            <img src="@/assets/icons/checkbox2.svg" alt="Школа тариф" itemprop="image"/>
           </div>
-          <p class="tarifs-section__tariftitle">Школа</p>
-          <h3 class="tarifs-section__func-title">Расчитывается индивидуально</h3>
+          <p class="tarifs-section__tariftitle" itemprop="name">Школа</p>
+          <h3 class="tarifs-section__func-title" itemprop="price">Рассчитывается индивидуально</h3>
 
           <div class="tarifs-section__tarif-features">
             <CheckboxItem description="Бизнес аккаунт" />
@@ -106,16 +107,16 @@ onMounted(() => {
 
           <button class="primary-button" @click="openFreeServiceOverlay">Выбрать</button>
         </div>
+
       </div>
 
-      <!-- Оверлей с информацией о бесплатном сервисе -->
       <div
         v-if="showFreeServiceOverlay"
         class="tarifs-section__free-service-overlay"
         @click="closeFreeServiceOverlay"
       >
         <div class="tarifs-section__free-service-content" @click.stop>
-          <h2 class="section-title">
+          <h2 class="section-title" itemprop="description">
             На текущий момент, наш сервис является бесплатным для всех пользователей
           </h2>
           <p class="desc inter-400">
@@ -132,10 +133,7 @@ onMounted(() => {
           </div>
         </div>
       </div>
+
     </div>
   </section>
 </template>
-
-<!-- <style lang="scss" scoped>
-// Стили перенесены в landing.scss по БЭМ. Этот блок можно удалить.
-</style> -->
