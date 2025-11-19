df551c6056f79734290a2a5559703da4b15d2bc3
Daniil Pavlov <foxy09876@yandex.ru>
2025-11-19 19:04:07 +0000

    SEO изменения #1 *Начало переработки страниц для SEO, изменение элементов и циклов, чтобы добавить SEO элементы (role, подсказки и др.)

--- src/components/newsPage/v-news.vue
+++ src/components/newsPage/v-news.vue
@@ -4,64 +4,40 @@
       <div class="container">
         <div class="v-news__container layout">
           <h1 class="v-news__title text-section-title">Корпоративные новости</h1>
-          <a href="https://t.me/teachandplan" target="_blank">
-            <div class="v-news__banner">
-              <!-- <div class="v-news__banner-block">
-                <h2 class="v-news__banner-title title">Присоединяетесь к нашему Telegram-каналу</h2>
-                <p class="v-news__banner-subtitle">Не пропускайте новости и обновления</p>
-                <a href="https://t.me/teachandplan" target="_blank" class="v-news__banner-button custom-btn white">
-                  <img src="/src/assets/images/telegram.svg" alt="" />
-                  Telegram-канал
-                </a>
-              </div>
-              <div class="v-news__banner-block">
-                <ul class="v-news__banner-list">
-                  <li class="v-news__banner-list-item">
-                    <a href="#!" class="contact-link"> #платформа </a> - рассказываем обо всех
-                    новостях платформы
-                  </li>
-                  <li class="v-news__banner-list-item">
-                    <a href="#!" class="contact-link"> #финансы </a> - как вести, как приумножить, как
-                    не потерять
-                  </li>
-                  <li class="v-news__banner-list-item">
-                    <a href="#!" class="contact-link"> #нововсти </a> - в сфере образования
-                  </li>
-                  <li class="v-news__banner-list-item">
-                    <a href="#!" class="contact-link"> #методика </a> - методические советы по
-                    достижению целей с учениками
-                  </li>
-                </ul>
-              </div>
 
-              <div class="v-news__banner-image">
-                <img src="/src/assets/images/iphone.png" alt="">
-              </div> -->
+          <a href="https://t.me/teachandplan" target="_blank" rel="noopener noreferrer" class="v-news__banner-link">
+            <div class="v-news__banner">
+              <!-- При необходимости можно вернуть содержимое баннера -->
             </div>
           </a>
 
           <ul class="v-news__list">
             <li class="v-news__list-item news">
-              <div class="news__image">
-                <img src="/src/assets/images/pupil.jpg" alt="" />
-              </div>
-              <div class="news__info">
-                <div class="news__info-block">
-                  <p class="news__date">1 июня 2025</p>
-                  <h2 class="news__title">Как не остаться без денег летом?</h2>
+              <article class="news__content">
+                <figure class="news__image">
+                  <img src="/src/assets/images/pupil.jpg" alt="Ученики на уроке" />
+                </figure>
+
+                <div class="news__info">
+                  <header class="news__info-block">
+                    <time class="news__date" datetime="2025-06-01">1 июня 2025</time>
+                    <h2 class="news__title">Как не остаться без денег летом?</h2>
+                  </header>
+
                   <p class="news__desc">
                     Если Вы не будете планировать заранее вашу деятельность, то в скором времени
-                    обнаружите, что летом учеников и заработка совсем нет. Так ли это? Конечно нет
-                    <br />
-                    <br />
+                    обнаружите, что летом учеников и заработка совсем нет. Так ли это? Конечно нет.
+                    <br /><br />
                     Чтобы такого не было, начинайте предпринимать шаги для снижения зависимости от
                     сезонности уже сейчас. Приглашаем вас на вебинар 1 мая. Зарегистрироваться можно
                     по кнопке ниже.
                   </p>
-                </div>
 
-                <button class="news__button custom-btn light-blue">Зарегистрироваться</button>
-              </div>
+                  <a href="#registration" class="news__button custom-btn light-blue" role="button">
+                    Зарегистрироваться
+                  </a>
+                </div>
+              </article>
             </li>
           </ul>
         </div>
@@ -69,6 +45,7 @@
     </section>
   </v-base>
 </template>
+
 <script setup>
 import vBase from '../v-base.vue'
 </script>
