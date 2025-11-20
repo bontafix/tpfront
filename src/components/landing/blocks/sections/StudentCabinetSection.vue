616167ceaf0ab8fd9da21384cd9e6721ab5f3742
Daniil Pavlov <foxy09876@yandex.ru>
2025-11-20 09:58:32 +0000

    SEO изменения #2

--- src/components/landing/blocks/sections/StudentCabinetSection.vue
+++ src/components/landing/blocks/sections/StudentCabinetSection.vue
@@ -1,5 +1,8 @@
 <template>
   <section class="student-cabinet-section" id="student" itemscope itemtype="https://schema.org/Product">
+    <meta itemprop="name" content="Кабинет ученика Teacher Planner" />
+    <meta itemprop="description" content="Единое пространство для ученика, где хранятся домашние задания, расписание занятий и отчеты для родителей" />
+    
     <h2 class="section-title inter-500 student-cabinet-section__title" itemprop="name">Кабинет ученика</h2>
     <p class="desc inter-400 student-cabinet-section__desc" itemprop="description">
       Единое пространство для ученика, где хранятся домашние задания, расписание занятий и отчеты для родителей
@@ -7,6 +10,9 @@
 
     <div class="student-cabinet-section__container">
       <div class="student-cabinet-section__container-item" itemprop="feature" itemscope itemtype="https://schema.org/Service">
+        <meta itemprop="name" content="Задавайте домашнее задание" />
+        <meta itemprop="description" content="Разгрузите ваши мессенджеры и сделайте задачу домашнего задания быстрой и удобной для вас и ваших учеников." />
+        <meta itemprop="image" content="../../../../assets/images/cabinet-section-demo.jpg" />
         <div class="student-cabinet-section__primary-item">
           <img src="../../../../assets/images/cabinet-section-demo.jpg" class="student-cabinet-section__primary-image" itemprop="image" alt="Задавайте домашнее задание">
           <h3 class="inter-500 student-cabinet-section__title-h3" itemprop="name">Задавайте домашнее задание</h3>
@@ -18,6 +24,9 @@
 
       <div class="student-cabinet-section__container-item">
         <div class="student-cabinet-section__feature-item feature-item" itemprop="feature" itemscope itemtype="https://schema.org/Service">
+          <meta itemprop="name" content="Гибкая система напоминаний для ученика и учителя" />
+          <meta itemprop="description" content="Проведите урок и задайте домашнее задание, а мы бережно напомним ученику о необходимости оплатить занятие и сдать домашнее задание на проверку." />
+          <meta itemprop="image" content="../../../../assets/images/not_demo.png" />
           <img class="notifications-image student-cabinet-section__secondary-image" src="../../../../assets/images/not_demo.png" itemprop="image" alt="Гибкая система напоминаний">
           <h3 class="inter-500 student-cabinet-section__title-h3" itemprop="name">Гибкая система напоминаний для ученика и учителя</h3>
           <p class="item-desc inter-400 student-cabinet-section__desc-p" itemprop="description">
@@ -26,6 +35,9 @@
         </div>
 
         <div class="student-cabinet-section__feature-item feature-item" itemprop="feature" itemscope itemtype="https://schema.org/Service">
+          <meta itemprop="name" content="Отправляйте отчеты родителям" />
+          <meta itemprop="description" content="Ведите записи по каждому занятию, а родителю они будут автоматически доступны в кабинете ученика." />
+          <meta itemprop="image" content="../../../../assets/images/ts_demo.png" />
           <img class="teacher-demo-image student-cabinet-section__secondary-image" src="../../../../assets/images/ts_demo.png" itemprop="image" alt="Отправляйте отчеты родителям">
           <h3 class="inter-500 student-cabinet-section__title-h3" itemprop="name">Отправляйте отчеты родителям</h3>
           <p class="item-desc inter-400 student-cabinet-section__desc-p" itemprop="description">
