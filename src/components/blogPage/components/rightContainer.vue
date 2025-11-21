419f907bbf64888b644e06457075510d57c0ef6a
Daniil Pavlov <foxy09876@yandex.ru>
2025-11-21 11:43:12 +0000

    shema (SEO)

--- src/components/blogPage/components/rightContainer.vue
+++ src/components/blogPage/components/rightContainer.vue
@@ -1,97 +1,104 @@
 <template>
-    <div class="right">
-        <h3 class="right__title">Последние статьи</h3>
+  <aside class="sidebar-latest-blogs" itemscope itemtype="https://schema.org/ItemList">
+    <h3 class="sidebar-latest-blogs__title">Последние статьи</h3>
 
-        <div class="right__container">
-            <div v-for="item in blogs" :key="item.id" class="right__blog" @click="() => openBlogPage(item.id)">
-                <p class="right__date">{{ new Date(item.published_at).toLocaleDateString('ru-RU') }}</p>
-                <h4 class="right__text">{{ item.title }}</h4>
-            </div>
-        </div>
+    <div class="sidebar-latest-blogs__container">
+      <div
+        v-for="(item, index) in blogs"
+        :key="item.id"
+        class="sidebar-latest-blogs__item"
+        @click="() => openBlogPage(item.id)"
+        :itemprop="'itemListElement'"
+        itemscope
+        itemtype="https://schema.org/BlogPosting"
+      >
+        <meta itemprop="position" :content="index + 1" />
+        <time class="sidebar-latest-blogs__date" :datetime="item.published_at" itemprop="datePublished">
+          {{ new Date(item.published_at).toLocaleDateString('ru-RU') }}
+        </time>
+        <h4 class="sidebar-latest-blogs__text" itemprop="headline">{{ item.title }}</h4>
+      </div>
     </div>
+  </aside>
 </template>
 
 <script setup>
 import { getNews } from '@/api/requests'
-import { onMounted, ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const blogs = ref([])
 
 function openBlogPage(id) {
-    window.location.href = `/blog/${id}`
+  window.location.href = `/blog/${id}`
 }
 
 async function getBlogs() {
-    const result = await getNews()
-    blogs.value = result.slice(0, 5)
+  const result = await getNews()
+  blogs.value = result.slice(0, 5)
 }
 
 onMounted(async () => {
-    await getBlogs()
+  await getBlogs()
 })
 </script>
 
 <style scoped>
-.right {
-    background-color: #F8F9FB;
-    padding: 30px 36px 44px 36px;
-    border: 1px solid #E9EAEB;
-    border-radius: 12px;
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    width: 100%;
-    max-width: 455px;
-    align-self: flex-start;
+.sidebar-latest-blogs {
+  background-color: #f8f9fb;
+  padding: 30px 36px 44px 36px;
+  border: 1px solid #e9eaeb;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  width: 100%;
+  max-width: 455px;
+  align-self: flex-start;
 }
 
-.right__title {
-    font-family: Inter;
-    font-weight: 600;
-    font-style: Semi Bold;
-    font-size: 20px;
-    line-height: 140%;
-    color: #344055;
-    padding-bottom: 18px;
-    border-bottom: 2px solid #1D4ECC;
+.sidebar-latest-blogs__title {
+  font-family: Inter, sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 140%;
+  color: #344055;
+  padding-bottom: 18px;
+  border-bottom: 2px solid #1d4ecc;
 }
 
-.right__container {
-    display: flex;
-    flex-direction: column;
-    gap: 28px;
+.sidebar-latest-blogs__container {
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
 }
 
-
-.right__blog {
-    padding-top: 22px;
-    border-top: 1px solid #E9EAEB;
-    display: flex;
-    flex-direction: column;
-    gap: 6px;
-    cursor: pointer;
+.sidebar-latest-blogs__item {
+  padding-top: 22px;
+  border-top: 1px solid #e9eaeb;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  cursor: pointer;
 }
 
-.right__blog:first-of-type {
-    padding-top: 0;
-    border-top: unset;
+.sidebar-latest-blogs__item:first-of-type {
+  padding-top: 0;
+  border-top: unset;
 }
 
-.right__date {
-    font-family: Inter;
-    font-weight: 300;
-    font-style: Light;
-    font-size: 14px;
-    line-height: 144%;
-    color: #344055;
+.sidebar-latest-blogs__date {
+  font-family: Inter, sans-serif;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 144%;
+  color: #344055;
 }
 
-.right__text {
-    font-family: Inter;
-    font-weight: 500;
-    font-style: Medium;
-    font-size: 17px;
-    line-height: 140%;
-    color: #344055;
+.sidebar-latest-blogs__text {
+  font-family: Inter, sans-serif;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 140%;
+  color: #344055;
 }
 </style>
