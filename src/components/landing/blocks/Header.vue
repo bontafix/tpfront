1bf418b5e30c13f58cf01c9b8c6ee3149d206a7d
Daniil Pavlov <foxy09876@yandex.ru>
2025-11-21 20:05:32 +0000

    Header backup

--- src/components/landing/blocks/Header.vue
+++ src/components/landing/blocks/Header.vue
@@ -1,25 +1,20 @@
 <template>
   <header class="landing-header">
-    <a href="/" class="logo-link">
-      <img src="../../../assets/icons/icon.svg" class="logo" alt="Teacher Planner Logo" />
-    </a>
+    <img src="../../../assets/icons/icon.svg" class="logo" alt="logo" />
 
-    <nav class="navigation mobile-hidden">
+    <div class="mobile-hidden navigation-buttons">
       <a href="#functions" class="navigation-button">Функции</a>
       <a href="#tarifs" class="navigation-button">Тарифы</a>
       <a href="#reviews" class="navigation-button">Отзывы</a>
       <a href="#faq" class="navigation-button">FAQ</a>
-    </nav>
-
-    <div class="auth-buttons">
-      <button v-show="!userAuth" class="primary-button login-button" @click="pushToLogin">Вход</button>
-      <button v-show="userAuth" class="navigation-button bold" @click="pushToLogin">Главная</button>
     </div>
+    <button v-show="!userAuth" class="primary-button login-button" @click="pushToLogin">Вход</button>
+    <button v-show="userAuth" class="navigation-button bold" @click="pushToLogin">Главная</button>
   </header>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
+<script setup>
+import { ref, computed, onMounted } from 'vue'
 import router from '@/router'
 import { isUserAuth } from '@/utils'
 
@@ -27,13 +22,17 @@ const userAuth = ref(false)
 
 function pushToLogin() {
   if(userAuth.value) {
-    router.push({ name: 'home_teacher' })
+    router.push({
+      name: 'home_teacher',
+    })
   } else {
-    router.push({ name: 'login' })
+      router.push({
+      name: 'login',
+    })
   }
 }
 
-onMounted(() => {
+onMounted(()=>{
   isUserAuth().then(authenticated => {
     userAuth.value = authenticated
   })
